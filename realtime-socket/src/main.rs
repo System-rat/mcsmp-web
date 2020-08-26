af91543e274429a7a4db9fb2b710147ffb27f753
System.rat <system.rodent@gmail.com>
2020-08-26 07:42:30 +0200

    CHange some things

--- realtime-socket/src/main.rs
+++ realtime-socket/src/main.rs
@@ -4,20 +4,19 @@ use lapin::{Connection, ConnectionProperties};
 use structopt::StructOpt;
 use tokio::net::TcpListener;
 use tokio_amqp::LapinTokioExt;
-use tokio_tungstenite::accept_async;
+use tokio_tungstenite::accept_hdr_async;
+use tokio_tungstenite::tungstenite::handshake::server::{ErrorResponse, Request, Response};
+use tokio_tungstenite::tungstenite::http::StatusCode;
 
 mod cli;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts = RealtimeCli::from_args();
-    let mut server: TcpListener = TcpListener::bind(format!("{}:{}", opts.binding, opts.port))
-        .await
-        .unwrap();
-    let con = Connection::connect(&opts.rabbitmq, ConnectionProperties::default().with_tokio())
-        .await
-        .unwrap();
-    con.create_channel().await.unwrap();
+    let mut server = TcpListener::bind(format!("{}:{}", opts.binding, opts.port)).await?;
+    let con =
+        Connection::connect(&opts.rabbitmq, ConnectionProperties::default().with_tokio()).await?;
+    let _chan = con.create_channel().await?;
     tokio::spawn(async {
         tokio::signal::ctrl_c().await.unwrap();
         println!("Woah there!");
@@ -25,12 +24,34 @@ async fn main() {
     });
 
     loop {
-        let (socket, _) = server.accept().await.unwrap();
+        let (socket, addr) = server.accept().await.unwrap();
         tokio::spawn(async move {
-            let mut websocket = accept_async(socket).await.unwrap();
-            for msg in websocket.next().await {
-                println!("Got message: {}", msg.unwrap());
+            let mut websocket = accept_hdr_async(socket, handle_connection)
+                .await
+                .map_err(|err| println!("An error occured: {}", err))
+                .unwrap();
+            while let Some(msg) = websocket.next().await {
+                println!("Got message from {}: {}", addr, msg.unwrap());
             }
         });
+    }
+}
+
+fn handle_connection(request: &Request, response: Response) -> Result<Response, ErrorResponse> {
+    println!(
+        "Connection received: {}",
+        match request.uri().path_and_query() {
+            Some(pq) => pq.as_str(),
+            None => "",
+        }
+    );
+
+    if request.uri().path().starts_with("/secret_code123") {
+        Ok(response)
+    } else {
+        Err(Response::builder()
+            .status(StatusCode::FORBIDDEN)
+            .body(Some("Access denied".into()))
+            .unwrap())
     }
 }
